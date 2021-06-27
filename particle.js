866d79ecee96339daf13724036004e0e46085a1a
MISCHELLE-HENRIQUES <77055896+MISCHELLE-HENRIQUES@users.noreply.github.com>
2021-06-27 12:38:07 +0530

    Update particle.js

--- particle.js
+++ particle.js
@@ -4,16 +4,16 @@ class Particle{
             restitution: 0
         }
         this.body = Bodies.circle(x,y,radius,options);
-        this.color=color(random(0, 255), random(0, 255), random(0, 255));
-        this.radius=10;
+        this.color=color(random(0, 255), random(0, 255), random(0, 255))
         World.add(world, this.body);
     }
 
     display(){
         var pos = this.body.position;
         push();
+        translate(pos.x , pos.y);
         noStroke();
-        translate(pos.x,pos.y);
+        rotate(angle)
        
         fill(this.color);
         ellipse(0,0,this.radius*2);
